912ab8bd668ba23b881eeb1b03632a46232d9023
sayeedzh <sayeed.705@gmail.com>
2024-06-13 20:40:56 +0530

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    home page
+    <Button>Click Me</Button>
+  </main>
+</template>
